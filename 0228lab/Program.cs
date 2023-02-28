df836a254857c074a7bf400bcc3271f4de98838f
Oleksandra <predictormagnolia@gmail.com>
2023-02-28 14:24:17 +0200

    Add project files.

---
+++ 0228lab/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0228lab
+{
+    class Program
+    {
+        public static void ToCountWords(string Text)
+        {
+            Dictionary<string, int> UniqueWordCounter = new Dictionary<string, int>(0);
+            string[] TextArray = Text.Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string str in TextArray)
+            {
+                if (UniqueWordCounter.ContainsKey(str) == true)
+                {
+                    UniqueWordCounter[str]++;
+                }
+                else
+                {
+                    UniqueWordCounter.Add(str, 1);
+                }
+            }
+            foreach (KeyValuePair<string, int> keyValue in UniqueWordCounter)
+            {
+                Console.WriteLine(keyValue.Key + " : " + keyValue.Value);
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your text: ");
+            string Text = Console.ReadLine();
+            ToCountWords(Text);
+
+        }
+    }
+}
